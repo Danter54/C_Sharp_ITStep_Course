0b00c916fdc8d462b99d25b1bd418a7a67c7ac9a
38066 <38066@DESKTOP-F1NHGC8>
2023-12-02 20:12:07 +0200

    Add Task1 and create repository

---
+++ HomeWork1/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = 0;
+
+while (true)
+{
+    Console.Write("Введите число >> ");
+    number = int.Parse(Console.ReadLine());
+
+    if (number < 1 || number > 100) Console.WriteLine("Ошибка!!!");
+    else if (number % 3 == 0 && number % 5 == 0) Console.WriteLine("Fizz Buzz");
+    else if (number % 3 == 0) Console.WriteLine("Fizz");
+    else if (number % 5 == 0) Console.WriteLine("Buzz");
+    else Console.WriteLine(number.ToString());
+}

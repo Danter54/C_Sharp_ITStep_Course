cbdbdfdcffe445ade51f06267bda0abcd0df9908
38066 <38066@DESKTOP-F1NHGC8>
2023-12-11 12:23:34 +0200

    Add HomeWork4

---
+++ HomeWork3/3/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp1;
+using System.Drawing;
+
+Squere s1 = new Squere(5f, "Tolik");
+
+string[] colorNames = { "red", "green", "blue" };
+ConsoleColor[] colorItems = { ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Blue };
+int idx = -1;
+int userChoose = 0;
+
+do
+{
+    Console.Clear();
+    for (int i = 0; i < colorNames.Length; i++)
+    {
+        if (userChoose == i)
+        {
+            Console.ForegroundColor = colorItems[userChoose];
+            Console.Write("-->");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        Console.WriteLine(colorNames[i]);
+    }
+
+    if (Console.ReadKey().Key == ConsoleKey.UpArrow)
+        userChoose -= 1;
+    if (Console.ReadKey().Key == ConsoleKey.DownArrow)
+        userChoose += 1;
+
+    if (userChoose < 0) { idx = 0; userChoose = 0; }
+    else if (userChoose > 2) { idx = 2; userChoose = 2; }
+    else idx = userChoose;
+
+} while (Console.ReadKey().Key != ConsoleKey.Enter);
+
+
+Console.Clear();
+
+Console.ForegroundColor = colorItems[idx];
+Console.WriteLine(s1.GetPiture());
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
---
+++ HomeWork3/3/ConsoleApp1/Squere.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Squere
+    {
+        private float Length { get; set; }
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+
+        public Squere()
+        {
+            this.Length = 0.0f;
+            this.Name = "unnamed";
+        }
+        public Squere(float length = 0.0f, string name = "unnamed")
+        {
+            this.Length = length;
+            this.Name = name;
+        }
+
+        public string GetInfo() { return $"The name of the squere is {Name}, length of the side is {Length}"; }
+
+        public string GetPiture()
+        {
+            string pict = new string("");
+
+            for (int i = 0; i < Math.Ceiling(Length); i++)
+            {
+                string tmp = "";
+                for (int j = 0; j < Math.Ceiling(Length); j++)
+                {
+                    if (i == 0 || i == Math.Ceiling(Length) - 1)
+                    {
+                        tmp += "*";
+                    }
+                    else if (j == 0 || j == Math.Ceiling(Length) - 1)
+                    {
+                        tmp += "*";
+                    }
+                    else tmp += ' ';
+                }
+                pict += tmp + '\n';
+            }
+
+            return pict;
+        }
+
+        public float GetSquere() { return Length * Length; }
+        public float GetPerimeter() { return 4.0f * Length; }
+
+    }
+}
